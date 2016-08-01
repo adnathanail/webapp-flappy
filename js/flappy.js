f5fd3bd3f5efafd64ae960fe022b0bc6a63671a0
Alexander Nathanail <adnathanail@MacBook-Pro.local>
2016-08-01 10:41:22 +0100

    Finished module 3

--- js/flappy.js
+++ js/flappy.js
@@ -9,19 +9,38 @@ var stateActions = { preload: preload, create: create, update: update };
 // - actions on the game state (or null for nothing)
 var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
 
+
+var score;
+score = 0;
+var labelScore;
+var player;
 /*
  * Loads all resources for the game and gives them names.
  */
 function preload() {
-
-
+  game.load.image("playerImg", "../assets/jamesBond.gif");
+  game.load.audio("score", "../assets/point.ogg");
 }
 
 /*
  * Initialises the game. This function is only called once.
  */
 function create() {
-    // set the background colour of the scene
+  game.stage.setBackgroundColor("#F3D3A3");
+  game.add.text(150, 160, "FLAPPY BOND!",{font: "50px Courier", fill: "#9B9B9B"});
+  game.add.sprite(10, 10, "playerImg");
+  game.add.sprite(750, 10, "playerImg");
+  game.add.sprite(750, 360, "playerImg");
+  game.add.sprite(10, 360, "playerImg");
+  //game.input.onDown.add(clickHandler);
+  game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(spaceHandler);
+  alert(score);
+  labelScore = game.add.text(20, 20, "0");
+  player = game.add.sprite(100, 200, "playerImg");
+  game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(moveRight);
+  game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(moveLeft);
+  game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(moveUp);
+  game.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(moveDown);
 }
 
 /*
@@ -29,4 +48,26 @@ function create() {
  */
 function update() {
 
+}
+function clickHandler(event) {
+  game.add.sprite(event.x-15, event.y-15, "playerImg");
+}
+function spaceHandler() {
+    game.sound.play("score");
+}
+function changeScore() {
+	score = score + 1;
+  labelScore.setText(score.toString());
+}
+function moveRight() {
+	player.x += 10;
+}
+function moveLeft() {
+	player.x -= 10;
+}
+function moveUp() {
+	player.y -= 10;
+}
+function moveDown() {
+	player.x += 10;
 }
