9ed00c0b6ea8d7a318e7ecb49fedd064e2a791cf
Alexander Nathanail <adnathanail@hotmail.co.uk>
2016-08-01 16:48:03 +0100

    Added states (menu)

--- js/flappy.js
+++ js/flappy.js
@@ -1,72 +1,102 @@
 //Magic constants
-var width = 790;
-var height = 400;
+var width = 640;
+var height = 480;
 var gravityValue = 800;
 var gameSpeed = -300;
 var gapSize = 100;
 var gapMargin = 50;
 var blockHeight = 50;
-//Setup phaser
-var stateActions = { preload: preload, create: create, update: update };
-var game = new Phaser.Game(width, height, Phaser.AUTO, 'game', stateActions);
 //Initialise variables
 var score = -1;
 var labelScore;
 var player;
 var pipes = [];
 var song;
-//Stuff that is needed to be loaded
-function preload() {
-  //Load pictures
-  game.load.image("playerImg", "../assets/nyan.png");
-  game.load.image("tail", "../assets/tail.png");
-  game.load.image("pipeBlock","../assets/pipe_pink.png");
-  //Load sounds
-  game.load.audio("score", "../assets/point.ogg");
-  game.load.audio("song", "../assets/nyansong.ogg");
-}
-//Stuff that is run once at the start
-function create() {
-  //Add player
-  player = game.add.sprite((width/2)-15, (height/2)-15, "playerImg");
-  //Add physics
-  game.physics.startSystem(Phaser.Physics.ARCADE);
-  game.physics.arcade.enable(player);
-  player.body.gravity.y = gravityValue;
-  //Add music
-  song = game.add.audio('song');
-  song.play('',0,1,true);
-  song.pause();
-  //Colour the background
-  game.stage.setBackgroundColor("#124376");
-  //Add score
-  labelScore = game.add.text(20, 20, "0");
-  //Add control
-  game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump);
-  game.input.keyboard.addKey(Phaser.Keyboard.P).onDown.add(pause);
-  //Add pipes
-  generatePipe();
-  var pipeInterval = 1.75 * Phaser.Timer.SECOND;
-  game.time.events.loop(pipeInterval,generatePipe);
-  //Change player anchor for rotation
-  player.anchor.setTo(0.5, 0.5);
-}
-//Continually runs throughout the game
-function update() {
-  //Collision and edge detection
-  game.physics.arcade.overlap(player,pipes,gameOver);
-  if(player.y > (height-25) || player.y < 0){
-    gameOver();
+var bootState = {
+  create: function(){
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.state.start('load');
   }
-  //Add tail
-  var tail = game.add.sprite(player.x-15,player.y-10,"tail");
-  player.bringToTop();
-  game.physics.arcade.enable(tail);
-  tail.body.velocity.x = gameSpeed;
-  tail.body.gravity.y = gravityValue/2;
-  //Rotate player to face direction of travel
-  player.rotation = Math.atan(player.body.velocity.y / 200);
-}
+};
+var loadState = {
+  preload: function(){
+    var loading = game.add.text(80,140, 'loading...', {font:'30px Courier', fill:'#ffffff'});
+    //Load pictures
+    game.load.image("playerImg", "../assets/nyan.png");
+    game.load.image("tail", "../assets/tail.png");
+    game.load.image("pipeBlock","../assets/pipe_pink.png");
+    game.load.image("mute","../assets/mute.png");
+    //Load sounds
+    game.load.audio("song", "../assets/nyansong.ogg");
+    game.state.start('menu');
+  }
+};
+var menuState = {
+  create: function(){
+    var nameLabel = game.add.text(80,80, 'Flappy Nyan', {font:'50px Arial', fill:'#ffffff'});
+    var startLabel = game.add.text(80,game.world.height-80, 'Click to start', {font:'25px Arial', fill:'#ffffff'});
+    game.input.onDown.add(function(){game.state.start('play');});
+  }
+};
+var playState = {
+  create: function(){
+    //Add player
+    player = game.add.sprite((width/2)-15, (height/2)-15, "playerImg");
+    //Add physics
+    game.physics.arcade.enable(player);
+    player.body.gravity.y = gravityValue;
+    //Add music
+    song = game.add.audio('song');
+    song.play('',0,1,true);
+    song.pause();
+    //Colour the background
+    game.stage.setBackgroundColor("#124376");
+    //Add 'mute' button
+    mute = game.add.button(750, 10, 'mute', function(){song.volume += 1;}, this);
+    //mute = new Phaser.Button(game, 200, 200, "mute", function(){song.volume += 1;},this);
+    //Add score
+    labelScore = game.add.text(20, 20, "0");
+    //Add control
+    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump);
+    game.input.keyboard.addKey(Phaser.Keyboard.P).onDown.add(pause);
+    //Add pipes
+    generatePipe();
+    var pipeInterval = 1.75 * Phaser.Timer.SECOND;
+    game.time.events.loop(pipeInterval,generatePipe);
+    //Change player anchor for rotation
+    player.anchor.setTo(0.5, 0.5);
+  },
+  update: function(){
+    //Collision and edge detection
+    game.physics.arcade.overlap(player,pipes,gameOver);
+    if(player.y > (height-25) || player.y < 0){
+      gameOver();
+    }
+    //Add tail
+    var tail = game.add.sprite(player.x-15,player.y-10,"tail");
+    player.bringToTop();
+    game.physics.arcade.enable(tail);
+    tail.body.velocity.x = gameSpeed;
+    tail.body.gravity.y = gravityValue/2;
+    //Rotate player to face direction of travel
+    player.rotation = Math.atan(player.body.velocity.y / 200);
+  }
+};
+var loseState = {
+  create: function(){
+    var nameLabel = game.add.text(80,80, 'Final score: ' + score.toString(), {font:'50px Arial', fill:'#ffffff'});
+    var startLabel = game.add.text(80,game.world.height-80, 'Click to restart', {font:'25px Arial', fill:'#ffffff'});
+    game.input.onDown.add(function(){game.state.start('play');});
+  }
+};
+//Setup phaser
+var game = new Phaser.Game(width, height, Phaser.AUTO, 'game');
+game.state.add('boot',bootState);
+game.state.add('load',loadState);
+game.state.add('menu',menuState);
+game.state.add('play',playState);
+game.state.add('lose',loseState);
+game.state.start('boot');
 //Update score variable and on screen
 function changeScore() {
   score = score + 1;
@@ -75,8 +105,8 @@ function changeScore() {
 //Generate pipe
 function generatePipe() {
   //Start of gap
-  var gapStart = game.rnd.integerInRange(gapMargin, height-gapSize-gapMargin);
-  for(var topCount=gapStart;topCount>0;topCount-=blockHeight){
+  var gapStart = game.rnd.integerInRange(gapMargin, height-gapSize-gapMargin-100);
+  for(var topCount=gapStart;topCount>-100;topCount-=blockHeight){
     addPipeBlock(width,topCount-blockHeight);
   }
   for(var bottomCount=gapStart+gapSize;bottomCount<height;bottomCount+=blockHeight) {
@@ -93,6 +123,8 @@ function addPipeBlock(x, y) {
   //Add physics to pipes
   game.physics.arcade.enable(block);
   block.body.velocity.x = gameSpeed;
+  block.body.velocity.y = 50;
+  game.time.events.loop(Phaser.Timer.SECOND, function(){block.body.velocity.y = -block.body.velocity.y;}, this);
 }
 function playerJump() {
   //Make it jump
@@ -106,7 +138,7 @@ function playerJump() {
   }
 }
 function gameOver() {
-  location.reload();
+  game.state.start('lose');
 }
 function pause(){
   game.paused = !game.paused;
