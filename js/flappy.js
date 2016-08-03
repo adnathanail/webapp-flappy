bb7e04105cb89c76c554724fbbd7f0a5f7bbabdb
Alexander Nathanail <adnathanail@hotmail.co.uk>
2016-08-03 02:00:36 +0100

    background and powerups

--- js/flappy.js
+++ js/flappy.js
@@ -12,6 +12,8 @@ var labelScore;
 var player;
 var pipes;
 var song;
+var balloons = [];
+var weights = [];
 var bootState = {
   create: function(){
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -27,6 +29,9 @@ var loadState = {
     game.load.image("pipeBlock","../assets/pipe2-body.png");
     game.load.image("pipeEnd","../assets/pipe2-end.png");
     game.load.image("mute","../assets/mute.png");
+    game.load.image("balloon","../assets/balloons.png");
+    game.load.image("weight","../assets/weight.png");
+    game.load.spritesheet('background', 'assets/background.png', 202, 202);
     //Load sounds
     game.load.audio("song", "../assets/nyansong.ogg");
     game.state.start('menu');
@@ -43,6 +48,42 @@ var menuState = {
 };
 var playState = {
   create: function(){
+    var background00 = game.add.sprite(0, 0, 'background');
+    background00.animations.add('play');
+    background00.animations.play('play', 10, true);
+    var background10 = game.add.sprite(202, 0, 'background');
+    background10.animations.add('play');
+    background10.animations.play('play', 10, true);
+    var background20 = game.add.sprite(404, 0, 'background');
+    background20.animations.add('play');
+    background20.animations.play('play', 10, true);
+    var background30 = game.add.sprite(606, 0, 'background');
+    background30.animations.add('play');
+    background30.animations.play('play', 10, true);
+    var background01 = game.add.sprite(0, 202, 'background');
+    background01.animations.add('play');
+    background01.animations.play('play', 10, true);
+    var background11 = game.add.sprite(202, 202, 'background');
+    background11.animations.add('play');
+    background11.animations.play('play', 10, true);
+    var background21 = game.add.sprite(404, 202, 'background');
+    background21.animations.add('play');
+    background21.animations.play('play', 10, true);
+    var background31 = game.add.sprite(606, 202, 'background');
+    background31.animations.add('play');
+    background31.animations.play('play', 10, true);
+    var background02 = game.add.sprite(0, 404, 'background');
+    background02.animations.add('play');
+    background02.animations.play('play', 10, true);
+    var background12 = game.add.sprite(202, 404, 'background');
+    background12.animations.add('play');
+    background12.animations.play('play', 10, true);
+    var background22 = game.add.sprite(404, 404, 'background');
+    background22.animations.add('play');
+    background22.animations.play('play', 10, true);
+    var background32 = game.add.sprite(606, 404, 'background');
+    background32.animations.add('play');
+    background32.animations.play('play', 10, true);
     score = -1;
     pipes = [];
     //Add player
@@ -50,20 +91,28 @@ var playState = {
     //Add physics
     game.physics.arcade.enable(player);
     player.body.gravity.y = gravityValue;
-    //Colour the background
-    game.stage.setBackgroundColor(18,67,118);
     //Add 'mute' button
     mute = game.add.button(600, 10, 'mute', function(){song.volume += 1;}, this);
     //mute = new Phaser.Button(game, 200, 200, "mute", function(){song.volume += 1;},this);
     //Add score
     labelScore = game.add.text(20, 20, "0");
     //Add control
-    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump);
+    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(function(){
+      //Make it jump
+      player.body.velocity.y = gameSpeed;
+      //Increase the incessant wailing
+      song.volume += 0.5;
+      if(song.isPlaying){
+        song.pause();
+      } else{
+        song.resume();
+      }
+    });
     game.input.keyboard.addKey(Phaser.Keyboard.P).onDown.add(pause);
     //Add pipes
     generatePipe();
     var pipeInterval = 2 * Phaser.Timer.SECOND;
-    game.time.events.loop(pipeInterval,generatePipe);
+    game.time.events.loop(pipeInterval,generate);
     //Change player anchor for rotation
     player.anchor.setTo(0.5, 0.5);
   },
@@ -81,6 +130,8 @@ var playState = {
     tail.body.gravity.y = gravityValue/4;
     //Rotate player to face direction of travel
     player.rotation = Math.atan(player.body.velocity.y / 200);
+    checkBonus(balloons, -50);
+    checkBonus(weights, 50);
   }
 };
 var loseState = {
@@ -140,17 +191,6 @@ function addPipeEnd(x, y) {
   block.body.velocity.y = 50;
   game.time.events.loop(Phaser.Timer.SECOND, function(){block.body.velocity.y = -block.body.velocity.y;}, this);
 }
-function playerJump() {
-  //Make it jump
-  player.body.velocity.y = gameSpeed;
-  //Increase the incessant wailing
-  song.volume += 0.5;
-  if(song.isPlaying){
-    song.pause();
-  } else{
-    song.resume();
-  }
-}
 function gameOver() {
   registerScore(score);
   game.state.start('lose');
@@ -160,6 +200,39 @@ function pause(){
   game.paused = !game.paused;
 }
 function changeGravity(g) {
-    gameGravity += g;
-    player.body.gravity.y = gameGravity;
+  gravityValue += g;
+  player.body.gravity.y = gravityValue;
+}
+function generateBalloons(){
+  var bonus = game.add.sprite(width, height, "balloon");
+  balloons.push(bonus);
+  game.physics.arcade.enable(bonus);
+  bonus.body.velocity.x = - 200;
+  bonus.body.velocity.y = - game.rnd.integerInRange(60, 100);
+}
+function generateWeights(){
+  var bonus = game.add.sprite(width, 0, "weight");
+  weights.push(bonus);
+  game.physics.arcade.enable(bonus);
+  bonus.body.velocity.x = - 200;
+  bonus.body.velocity.y = game.rnd.integerInRange(60, 100);
+}
+function generate(){
+  num = game.rnd.integerInRange(1,4);
+  if(num == 1){
+    generateBalloons();
+  } else if(num == 2){
+    generateWeights();
+  } else{
+    generatePipe();
+  }
+}
+function checkBonus(bonusArray, bonusEffect) {
+    for(var i = bonusArray.length - 1; i >= 0; i--){
+        game.physics.arcade.overlap(player, bonusArray[i], function(){
+            changeGravity(bonusEffect);
+            bonusArray[i].destroy();
+            bonusArray.splice(i,1);
+        });
+    }
 }
